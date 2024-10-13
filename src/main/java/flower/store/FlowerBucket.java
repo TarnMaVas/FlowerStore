bc57159146c480d3bd66601ba7078e12a5b26f9d
Maxim-Vasyl Tarnavskyi <tarnmavas@gmail.com>
2024-10-13 12:23:23 +0300

    FlowerPack/FlowerBucket update

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -1,5 +1,26 @@
 package flower.store;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlowerBucket {
+
+    private final List<FlowerPack> flowerPacks;
     
+    public FlowerBucket() {
+        this.flowerPacks = new ArrayList<>();
+
+    }
+
+    public void add(FlowerPack flowerPack) {
+        flowerPacks.add(flowerPack);
+    }
+
+    public double getPrice() {
+        double price = 0;
+        for (FlowerPack flowerPack: flowerPacks) {
+            price += flowerPack.getPrice();
+        }
+        return price;
+    }
 }
